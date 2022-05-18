40cab38b05ff93eb0ba6a534d9d79cd32fbbe5dd
larissa <laramina984@gmail.com>
2022-05-18 09:49:29 -0300

    atualizacao

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+   include('conexao.php');
+   $nome = $_POST['tNome1'];
+   echo '<p>Nome do Usuário: '.$nome;
+
+   $email = $_POST['tEmail1'];
+   echo '<p>E-mail: '.$email;
+
+   $telefone = $_POST['tTelefone1'];
+   echo '<p>Telefone: '.$telefone;
+
+   $sql = " INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+    VALUES ('".$nome. "','".$email."','".$telefone."')";
+     echo'<br>';
+     echo'<br>';
+    $result = mysqli_query($con, $sql);
+    if($result)
+      echo 'Dados inseridos com sucesso';
+    else
+    echo 'Erro ao inserir no banco de dados: '.mysqli_error($con);
+    
+        
+
+?>
+
+<p><a href='index.php'> Voltar</a></p>
