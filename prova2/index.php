76b3dd0e0bcd9612b16e7d625ad80ef664957873
larissa <laramina984@gmail.com>
2022-06-15 10:37:51 -0300

    c1

--- prova2/index.php
+++ prova2/index.php
@@ -13,7 +13,7 @@
         <ul>
             <li><a href="Cadastro_fluxo_caixa.html"> Cadastro Fluxo de Caixa</a></li>
             <li><a href="Listar_fluxo_caixa.php"> Listagem de Fluxo de Caixa</a></li>
-            <li><a href="Consulta_fluxo_caixa.html"> Consulta Saldo do Caixa</a></li>
+            <li><a href="Consulta_fluxo_caixa.html"> Consulta Saldo do Caixa</a></li>  
         </ul>
     </div>
 </body>
